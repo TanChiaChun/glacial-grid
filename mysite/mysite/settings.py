9c9e0923f1ea8c24aaf5526f67237af5683c56d9
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2024-02-12 20:36:18 +0800

    Add productivity app to INSTALLED_APPS

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS: list[str] = []
 # Application definition
 
 INSTALLED_APPS = [
+    "productivity.apps.ProductivityConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
