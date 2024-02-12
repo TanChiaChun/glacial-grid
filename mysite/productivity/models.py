c2946770ee8b1e970e31f10f6afcb55a8445e02d
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2024-02-12 20:18:58 +0800

    Add Productivity Model

---
+++ mysite/productivity/models.py
@@ -0,0 +1,22 @@
+"""Models for productivity app."""
+
+from django.db import models
+
+
+class Productivity(models.Model):
+    """Productivity model."""
+
+    class Frequency(models.IntegerChoices):
+        """Choices for Frequency field."""
+
+        KEY = 0
+        LOOP = 1
+        DAY = 2
+        WEEK = 3
+        MONTH = 4
+
+    item = models.CharField(max_length=200)
+    frequency = models.IntegerField(choices=Frequency.choices)
+    group = models.CharField(max_length=200)
+    last_check = models.DateTimeField(auto_now=True)
+    last_check_undo = models.DateTimeField()
