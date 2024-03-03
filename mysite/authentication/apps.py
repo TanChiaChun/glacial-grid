2f4453fc5dd9c84d03c82e81437da038a2122c10
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2024-03-03 20:29:04 +0800

    Apply changes per Pylint checks

--- mysite/authentication/apps.py
+++ mysite/authentication/apps.py
@@ -1,6 +1,10 @@
+"""Application configuration."""
+
 from django.apps import AppConfig
 
 
 class AuthenticationConfig(AppConfig):
+    """Application configuration objects, metadata for an application."""
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "authentication"
