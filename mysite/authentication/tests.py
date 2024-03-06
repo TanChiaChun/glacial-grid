3e2fdcd254ca3b3d94ed2f839f8365e992e761c6
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2024-03-07 07:06:32 +0800

    Add trailing slash to RequestFactory get path

--- mysite/authentication/tests.py
+++ mysite/authentication/tests.py
@@ -5,6 +5,6 @@ from authentication.views import acquire_csrf_token
 
 class ViewsTests(SimpleTestCase):
     def test_acquire_csrf_token(self) -> None:
-        request = RequestFactory().get("/authentication/csrftoken")
+        request = RequestFactory().get("/authentication/csrftoken/")
         response = acquire_csrf_token(request)
         self.assertIn("csrftoken", response.cookies)
