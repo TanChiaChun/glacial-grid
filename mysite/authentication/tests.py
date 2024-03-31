55d004e2c7addd99215c74cbf8e24bcd078b1fcc
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2024-03-31 19:10:45 +0800

    Login & get session ID via login/ POST

--- mysite/authentication/tests.py
+++ mysite/authentication/tests.py
@@ -1,11 +1,35 @@
 import json
+import string
+from random import choice
 
-from django.test import RequestFactory, SimpleTestCase
+from django.contrib.auth import get_user_model
+from django.test import Client, RequestFactory, TestCase
 
 from authentication.views import authentication_login, csrftoken
 
 
-class ViewsTests(SimpleTestCase):
+def generate_random_str(length: int = 8) -> str:
+    """Generate a random string.
+
+    - Consist of ASCII lowercase & uppercase and digits.
+
+    Args:
+        length:
+            Length of string to be generated.
+
+    Returns:
+        Random string.
+    """
+    str_seq = string.ascii_letters + string.digits
+
+    return "".join([choice(str_seq) for i in range(length)])
+
+
+class ViewsTests(TestCase):
+    def setUp(self) -> None:
+        self.username = generate_random_str()
+        self.password = generate_random_str()
+
     def test_authentication_login_get(self) -> None:
         request = RequestFactory().get("")
         response = authentication_login(request)
@@ -26,3 +50,38 @@ class ViewsTests(SimpleTestCase):
         response = csrftoken(request)
 
         self.assertIn("csrftoken", response.cookies)
+
+    def test_login_user(self) -> None:
+        get_user_model().objects.create_user(
+            self.username, password=self.password
+        )
+
+        response = Client().post(
+            "/authentication/login/",
+            data={"username": self.username, "password": self.password},
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertDictEqual(
+            json.loads(response.content), {"info": "Login success"}
+        )
+        self.assertIn("sessionid", response.cookies)
+
+    def test_login_user_fail_missing_data(self) -> None:
+        response = Client().post("/authentication/login/")
+
+        self.assertEqual(response.status_code, 400)
+        self.assertDictEqual(
+            json.loads(response.content), {"error": "Missing data"}
+        )
+
+    def test_test_login_user_fail_invalid_login(self) -> None:
+        response = Client().post(
+            "/authentication/login/",
+            data={"username": self.username, "password": self.password},
+        )
+
+        self.assertEqual(response.status_code, 401)
+        self.assertDictEqual(
+            json.loads(response.content), {"error": "Invalid login"}
+        )
--- mysite/authentication/views.py
+++ mysite/authentication/views.py
@@ -1,11 +1,12 @@
 """Views for authentication app."""
 
+from django.contrib.auth import authenticate, login
 from django.http import HttpRequest, JsonResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.decorators.http import require_http_methods
 
 
-@require_http_methods(["GET"])
+@require_http_methods(["GET", "POST"])
 def authentication_login(request: HttpRequest) -> JsonResponse:
     """URL for login redirection.
 
@@ -21,6 +22,8 @@ def authentication_login(request: HttpRequest) -> JsonResponse:
     """
     if request.method == "GET":
         json_response = JsonResponse({"error": "Login required"}, status=401)
+    elif request.method == "POST":
+        json_response = login_user(request)
 
     return json_response
 
@@ -37,3 +40,28 @@ def csrftoken(request: HttpRequest) -> JsonResponse:
         Empty JSON Response with CSRF token in cookie.
     """
     return JsonResponse({})
+
+
+def login_user(request: HttpRequest) -> JsonResponse:
+    """Login user & send Session cookie in Response.
+
+    Args:
+        request:
+            HttpRequest object.
+
+    Returns:
+        JSON Response or error message.
+    """
+    try:
+        username = request.POST["username"]
+        password = request.POST["password"]
+    except KeyError:
+        return JsonResponse({"error": "Missing data"}, status=400)
+
+    user = authenticate(username=username, password=password)
+    if user is None:
+        return JsonResponse({"error": "Invalid login"}, status=401)
+
+    login(request, user)
+
+    return JsonResponse({"info": "Login success"})
